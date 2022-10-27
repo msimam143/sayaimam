e2bb2d516f029ecd68502adbccf836e7ee5dd272
msimam <msimam143@gmail.com>
2022-10-28 02:15:03 +0700

    fist commit

--- website/website/settings.py
+++ website/website/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'DB_ENGINE': 'django.db.backends.mysql',
+        'DB_NAME': 'djangoproject',
+        'DB_USER':'root',
+        'DB_PASSWORD':'',
+        'DB_HOST':'localhost',
+        'DB_PORT':'3306',
     }
 }
 
